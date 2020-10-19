7cc146c6e2d441ba3660bc041b529b684bda0b30
ManDeep Singh <mandeeparora132@gmail.com>
2020-10-19 15:18:36 +0530

    Snakegame With Javascript (#10)
    * Javascript
    
    * Create readme.md
    
    * Snakegame with Javascript
    
    * Added Game Images
    
    * Updated Code
    
    * Update Readme with Screenshots
    
    * Added Live and End game score
    
    * Updated Assets path
    
    * Updated readme with steps to run the game
    
    * Updated Readme with Github Pages link
    
    Updated Readme to play the game online with github pages link.
    
    * Added Restart Function

---
+++ Javascript/readme.md
@@ -0,0 +1,28 @@
+# Snakegame with Javascript
+## Screenshots
+
+**Game**
+
+![GameRun](https://user-images.githubusercontent.com/18049003/94899571-32076500-04b1-11eb-9e60-b682010c1473.png)
+
+**Game Over**
+
+![GameOver](https://user-images.githubusercontent.com/18049003/94899558-2caa1a80-04b1-11eb-8e11-97f5acae64e2.png)
+
+**Game Full**
+
+![GameFull](https://user-images.githubusercontent.com/18049003/94899583-36338280-04b1-11eb-8676-87dd7f35463d.png)
+
+**Note: I Used PlayCode.io to compile the game, likewise you could use any online compiler or run it on your own web server or pc.**
+
+Playcode Project Url: https://playcode.io/684580/
+
+Also Available at: https://deepsingh132.github.io/snakegame/
+
+### **Steps to run the game :-**
+
+1) Download the source code and all the assets into a folder
+2) Open the index.html file on a browser
+3) Enjoy :)
+
+***Ensure that all the assets are in the same folder or the game won't run.**
---
+++ Javascript/snake.js
@@ -0,0 +1,246 @@
+var canvas;
+var ctx;
+
+var head;
+var apple;
+var ball;
+
+var score;
+
+var dots;
+var apple_x;
+var apple_y;
+
+var leftDirection = false;
+var rightDirection = true;
+var upDirection = false;
+var downDirection = false;
+var inGame = true;    
+
+var intvl;
+
+const DOT_SIZE = 10;
+const ALL_DOTS = 900;
+const MAX_RAND = 29;
+const DELAY = 140;
+const C_HEIGHT = 300;
+const C_WIDTH = 300;    
+
+const LEFT_KEY = 37;
+const RIGHT_KEY = 39;
+const UP_KEY = 38;
+const DOWN_KEY = 40;
+
+var x = new Array(ALL_DOTS);
+var y = new Array(ALL_DOTS);   
+
+
+function init() {
+    
+    canvas = document.getElementById('myCanvas');
+    canvas.style = "position:absolute; left: 50%; width: 400px; margin-left: -200px;";
+    ctx = canvas.getContext('2d');
+    
+    
+    loadImages();
+    createSnake();
+    locateApple();
+    setTimeout("gameCycle()", DELAY);
+}    
+
+function loadImages() {
+    
+    head = new Image();
+    head.src = 'head.png';    
+    
+    ball = new Image();
+    ball.src = 'dot.png'; 
+    
+    apple = new Image();
+    apple.src = 'apple.png'; 
+}
+
+function createSnake() {
+
+    dots = 3;
+    score=0;
+
+    for (var z = 0; z < dots; z++) {
+        x[z] = 50 - z * 10;
+        y[z] = 50;
+    }
+}
+
+function checkApple() {
+  
+    if ((x[0] == apple_x) && (y[0] == apple_y)) {
+
+        dots++;
+        //score++;
+        score=score+10;
+        locateApple();
+    }
+}    
+
+function doDrawing() {
+    
+    ctx.clearRect(0, 0, C_WIDTH, C_HEIGHT);
+    
+    if (inGame) {
+
+        ctx.drawImage(apple, apple_x, apple_y);
+        
+    ctx.fillStyle = '#40FF00';
+    ctx.textBaseline = 'middle'; 
+    ctx.textAlign = 'center'; 
+    ctx.font = 'normal bold 15px serif';
+    ctx.fillText("Score: "+score,C_WIDTH/1.2,C_HEIGHT/15);
+
+        for (var z = 0; z < dots; z++) {
+            
+            if (z == 0) {
+                ctx.drawImage(head, x[z], y[z]);
+            } else {
+                ctx.drawImage(ball, x[z], y[z]);
+            }
+        }    
+    } else {
+
+        gameOver();
+    }        
+}
+
+function gameOver() {
+    
+    ctx.fillStyle = 'white';
+    ctx.textBaseline = 'middle'; 
+    ctx.textAlign = 'center'; 
+    ctx.font = 'normal bold 18px serif';
+
+    ctx.fillText('Game over !', C_WIDTH/2, C_HEIGHT/2);
+    ctx.fillText('Your Score is:  ',C_WIDTH/2, C_HEIGHT/1.5);
+    ctx.fillText(score,C_WIDTH/1.4, C_HEIGHT/1.5);
+    ctx.fillText('Restarting in ',C_WIDTH/2, C_HEIGHT/1.2);
+    var num=5;
+    intvl=setInterval(function(){
+		counter(ctx,num--);
+		},1000);
+}
+
+ function counter(ctx, num) {
+
+if(num == 0){
+ clearInterval(intvl);
+ location.reload();
+ }
+ else{
+ ctx.clearRect(200,205,canvas.width,canvas.height);
+ ctx.fillStyle = "red";
+ ctx.textAlign = "center";
+ ctx.fillText(""+num, C_WIDTH/1.4, C_HEIGHT/1.2);
+ }
+ } 
+
+function move() {
+
+    for (var z = dots; z > 0; z--) {
+        x[z] = x[(z - 1)];
+        y[z] = y[(z - 1)];
+    }
+
+    if (leftDirection) {
+        x[0] -= DOT_SIZE;
+    }
+
+    if (rightDirection) {
+        x[0] += DOT_SIZE;
+    }
+
+    if (upDirection) {
+        y[0] -= DOT_SIZE;
+    }
+
+    if (downDirection) {
+        y[0] += DOT_SIZE;
+    }
+}    
+
+function checkCollision() {
+
+    for (var z = dots; z > 0; z--) {
+
+        if ((z > 4) && (x[0] == x[z]) && (y[0] == y[z])) {
+            inGame = false;
+        }
+    }
+
+    if (y[0] >= C_HEIGHT) {
+        inGame = false;
+    }
+
+    if (y[0] < 0) {
+       inGame = false;
+    }
+
+    if (x[0] >= C_WIDTH) {
+      inGame = false;
+    }
+
+    if (x[0] < 0) {
+      inGame = false;
+    }
+}
+
+function locateApple() {
+
+    var r = Math.floor(Math.random() * MAX_RAND);
+    apple_x = r * DOT_SIZE;
+
+    r = Math.floor(Math.random() * MAX_RAND);
+    apple_y = r * DOT_SIZE;
+}    
+
+function gameCycle() {
+    
+    if (inGame) {
+
+        checkApple();
+        checkCollision();
+        move();
+        doDrawing();
+        setTimeout("gameCycle()", DELAY);
+    }
+}
+
+onkeydown = function(e) {
+    
+    var key = e.keyCode;
+    
+    if ((key == LEFT_KEY) && (!rightDirection)) {
+        
+        leftDirection = true;
+        upDirection = false;
+        downDirection = false;
+    }
+
+    if ((key == RIGHT_KEY) && (!leftDirection)) {
+        
+        rightDirection = true;
+        upDirection = false;
+        downDirection = false;
+    }
+
+    if ((key == UP_KEY) && (!downDirection)) {
+        
+        upDirection = true;
+        rightDirection = false;
+        leftDirection = false;
+    }
+
+    if ((key == DOWN_KEY) && (!upDirection)) {
+        
+        downDirection = true;
+        rightDirection = false;
+        leftDirection = false;
+    }        
+};    
